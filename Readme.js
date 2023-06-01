51162945064a0deb4fd941364c2ea3e96b22495e
Vishal Kumar <viskumarbit001@gmail.com>
2023-06-01 21:28:41 +0530

    inital commit

---
+++ Readme.js
@@ -0,0 +1,8 @@
+/*
+    TODO: Query data from Firebase Firestore and Reactjs - Part 2 
+    ! 18:00 min
+    ? error in order page , fix by useEffect((), [!firebase])
+    ? ?.then().catch
+    npm run build
+    netlify
+*/
